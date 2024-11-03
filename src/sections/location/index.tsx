669db186e6c3080c290214ca767866ebb2eaf6b4
Nusri Samath <nusrisamath@gmail.com>
2024-11-03 07:04:51 +0530

    fix: location map

--- src/sections/location/index.tsx
+++ src/sections/location/index.tsx
@@ -16,7 +16,7 @@ const LocationSection = () => {
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.207981016192!2d79.89129827499599!3d6.8656622931329325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25bdd8cbb3adb%3A0x3bfd0a09d63e11d8!2sThe%20Samosa!5e0!3m2!1sen!2slk!4v1730371710789!5m2!1sen!2slk" width="100%" height="600" loading="lazy"></iframe>
                         </div>
                         <div className="w-full items-center justify-center md:hidden flex">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.207981016192!2d79.89129827499599!3d6.8656622931329325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25bdd8cbb3adb%3A0x3bfd0a09d63e11d8!2sThe%20Samosa!5e0!3m2!1sen!2slk!4v1730371710789!5m2!1sen!2slk" width="100%" height="200" loading="lazy"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.207981016192!2d79.89129827499599!3d6.8656622931329325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25bdd8cbb3adb%3A0x3bfd0a09d63e11d8!2sThe%20Samosa!5e0!3m2!1sen!2slk!4v1730371710789!5m2!1sen!2slk" width="100%" height="300" loading="lazy"></iframe>
                         </div>
                     </div>
                 </div>
