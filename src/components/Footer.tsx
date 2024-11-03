50d67002c04c1b6ec3a4af6be321dc90da00ff57
Nusri Samath <nusrisamath@gmail.com>
2024-11-03 08:13:43 +0530

    Updated: oepn app links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,9 @@ const Footer = () => {
         <React.Fragment>
             <footer className="bg-white shadow  dark:bg-gray-800">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://funkyfunction.vercel.app/" className="hover:underline">funckyFunctions™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://funkyfunction.vercel.app/" className="hover:underline">funkyFunctions™</a>. All Rights Reserved.
                     </span>
+                    <span>v </span>
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                         {/* <li>
                         <a href="#" className="hover:underline me-4 md:me-6">About</a>
