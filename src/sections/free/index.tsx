d77dd2e60b84abf202a8b4d898058aeea32ad249
Nusri Samath <nusrisamath@gmail.com>
2024-12-13 22:27:30 +0530

    feat: added baackground image

--- src/sections/free/index.tsx
+++ src/sections/free/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FreePage: React.FC = () => {
     return (
         <React.Fragment>
-            <div id="free" className='flex w-full h-screen items-center justify-center bg-yellow-400 text-center'>
+            <div id="free" className={`flex w-full h-screen items-center justify-center bg-yellow-400 text-center bg-[url('/samosa-bg.png')]`}>
                 <div className='modak-regular text-2xl md:text-8xl '>To grab a
                     <br />
                     <a href='https://linktr.ee/thesamosa' className='text-red-500 underline'>
--- src/sections/main/index.tsx
+++ src/sections/main/index.tsx
@@ -3,7 +3,7 @@ import myImage from '../../assets/main.png';
 const MainSection = () => {
     return (
         <React.Fragment>
-            <div className='flex w-full h-screen items-center justify-center bg-yellow-400'>
+            <div className={`flex w-full h-screen items-center justify-center bg-yellow-400`}>
                 <img src={myImage} alt="My Image" className=" md:max-w-full md:max-h-full md:object-cover" />
             </div>
         </React.Fragment>
